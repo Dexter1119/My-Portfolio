20b7b660a0e09cd03619b9087b33ffd37929f8b3
Dexter1119 <kalsaitpc@gmail.com>
2025-04-06 17:05:28 +0530

    hospital-food-management

---
+++ src/components/project.jsx
@@ -0,0 +1,12 @@
+import { PROJECTS } from "../constants"
+
+const Project=()=>{
+    return(
+        <div className="border-b border-neutral-900 pb-4">
+            <h1 className="my-20 text-centre text-4xl">Projects</h1>
+            <div>
+               
+            </div>
+        </div>
+    )
+}
